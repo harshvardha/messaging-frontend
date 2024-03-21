7a84af766c68072b1bc96f5dc62e9a4c78f47b8f
harshvardhan singh <harshvardhansingh458@protonmail.com>
2024-03-21 18:04:45 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+import Messaging from "./pages/Messaging/Messaging";
+import Profile from "./pages/Profile/Profile";
 
 function App() {
   return (
     <div className="App">
-      messaging app
+      <BrowserRouter>
+        <Routes>
+          <Route index path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/messaging" element={<Messaging />} />
+          <Route path="profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
