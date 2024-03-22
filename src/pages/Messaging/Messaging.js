733b7469ed4e34c2d520942cf9eb4383e3e34436
harshvardhan singh <harshvardhansingh458@protonmail.com>
2024-03-22 19:22:59 +0530

    created left side pane

--- src/pages/Messaging/Messaging.js
+++ src/pages/Messaging/Messaging.js
@@ -1,9 +1,52 @@
+import { useState } from "react";
+import { IoIosSearch } from "react-icons/io";
+import ProfileHeader from "../../components/ProfileHeader/ProfileHeader";
+import ChatUser from "../../components/ChatUser/ChatUser";
 import "./Messaging.css";
 
 const Messaging = () => {
+    const [searchQuery, setSearchQuery] = useState();
+
     return (
         <div className="messaging">
-            messaging
+            <div className="messaging--chatUsers">
+                <ProfileHeader />
+                <div className="chatUsers--search">
+                    <input
+                        id="chatUsers--searchConversations"
+                        type="text"
+                        value={searchQuery}
+                        onChange={(event) => setSearchQuery(event.target.value)}
+                        placeholder="Search or start a new chat"
+                    />
+                </div>
+                <div className="chatUsers--conversations">
+                    <ChatUser />
+                    <ChatUser />
+                    <ChatUser />
+                    <ChatUser />
+                    <ChatUser />
+                    <ChatUser />
+                    <ChatUser />
+                    <ChatUser />
+                    <ChatUser />
+                    <ChatUser />
+                    <ChatUser />
+                    <ChatUser />
+                    <ChatUser />
+                    <ChatUser />
+                    <ChatUser />
+                    <ChatUser />
+                    <ChatUser />
+                    <ChatUser />
+                    <ChatUser />
+                    <ChatUser />
+                    <ChatUser />
+                </div>
+            </div>
+            <div className="messaging--chatArea">
+
+            </div>
         </div>
     )
 }
